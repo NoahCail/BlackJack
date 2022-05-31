c32e4507f413fe0dd80631d7337406a5d5e5e3ad
Noah <noahcail96@gmail.com>
2022-05-31 00:20:06 -0400

    Broke up the start_game and declaration functions to have more single purposed functions.

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
 """Program for playing BlackJack/21 using Python"""
 import simp_funcs as sf
 
-sf.start_game(True)
+sf.start_game()
--- simp_funcs.py
+++ simp_funcs.py
@@ -1,8 +1,7 @@
 """Python Module containing all the main functions needed for the overall BlackJack program"""
 
-# TODO: Add in functionality for doubling down and splitting on pairs
+# TODO: Add in functionality for doubling down and splitting on pairs & insurance on dealer aces
 # TODO: Add in functionality for there to be a player and a balance. Also can decide how much to bet each hand
-# TODO: Build in functionality to be able to see the cards drawn by the dealer
 import random as r
 
 logo = """
@@ -20,19 +19,24 @@ logo_counter = 0
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
-def declaration(player_hand, comp_hand, final):
-    """Returns the current or final player and computer hands as well as their current or final scores"""
+# Returns the current scores and hands
+def current_dec(player_hand, comp_hand):
+    """Returns the current player and computer hands as well as their current scores"""
 
-    if final:
-        return f'Your final hand: {player_hand}, Final score: {sum(player_hand)} \n \
-        Computer\'s final hand: {comp_hand}, Final score: {sum(comp_hand)} \n'
-    else:
-        return f'Your current hand: {player_hand}, Current score: {sum(player_hand)} \n \
-        Computer\'s current hand: {comp_hand[1:]}, Current score: ? \n'
+    return f'Your current hand: {player_hand}, Current score: {sum(player_hand)} \n \
+            Computer\'s current hand: {comp_hand[1:]}, Current score: ? \n'
+
+
+# Returns the final scores and hands
+def final_dec(player_hand, comp_hand):
+    """Returns the final player and computer hands as well as their final scores"""
+
+    return f'Your final hand: {player_hand}, Final score: {sum(player_hand)} \n \
+    Computer\'s final hand: {comp_hand}, Final score: {sum(comp_hand)} \n'
 
 
 def deal_cards():
-    """Returns the initial hands of both the player and the """
+    """Returns the initial hands of both the player and the computer"""
 
     pc1, pc2 = r.choice(cards), r.choice(cards)
     cc1, cc2 = r.choice(cards), r.choice(cards)
@@ -42,32 +46,35 @@ def deal_cards():
     return [p_cards, c_cards]
 
 
-def start_game(first):
-    """Starts the black jack game and is reused to also restart the game after the initial play"""
+def start_game():
+    """Starts the initial black jack game"""
 
     answer = input('Would you like to play a game of Black Jack? Type "y" for Yes and "n" for No: ')
-    if answer == 'y':
-        if first:
-            print(logo)
-            p_hand, c_hand = deal_cards()
-            print(declaration(p_hand, c_hand, False))
-            card_q(p_hand, c_hand)
-        else:
-            p_hand, c_hand = deal_cards()
-            print(declaration(p_hand, c_hand, False))
-            card_q(p_hand, c_hand)
-    elif answer == 'n':
-        if first:
-            print('Then why did you start the program in the first place?')
-        else:
-            print('Thanks for playing!')
+    if answer.upper() == 'Y':
+        print(logo)
+        p_hand, c_hand = deal_cards()
+        print(current_dec(p_hand, c_hand))
+        card_q(p_hand, c_hand)
+    elif answer.upper() == 'N':
+        print('Then why did you start the program in the first place?')
     else:
-        if first:
-            print('Isn\'t it so tough to type a y or an n, no but seriously figure it out')
-            start_game(True)
-        else:
-            print('Isn\'t it so tough to type a y or an n, no but seriously figure it out')
-            start_game(False)
+        print('Isn\'t it so tough to type a y or an n, no but seriously figure it out')
+        restart_game()
+
+
+def restart_game():
+    """Restarts the black jack game after one of the outcomes is reached"""
+
+    answer = input('Would you like to play a game of Black Jack? Type "y" for Yes and "n" for No: ')
+    if answer.upper() == 'Y':
+        p_hand, c_hand = deal_cards()
+        print(current_dec(p_hand, c_hand))
+        card_q(p_hand, c_hand)
+    elif answer.upper() == 'N':
+        print('Thanks for playing!')
+    else:
+        print('Isn\'t it so tough to type a y or an n, no but seriously figure it out')
+        restart_game()
 
 
 def dealer_q(player_hand, comp_hand):
@@ -75,32 +82,28 @@ def dealer_q(player_hand, comp_hand):
     x, y = sum(player_hand), sum(comp_hand)
 
     if x > 21:
-        print('You went over 21. You lose.')
-        print(declaration(player_hand, comp_hand, True))
-        start_game(False)
+        print(f'You went over 21. You lose. \n {final_dec(player_hand, comp_hand)}')
+        restart_game()
     elif y > 21:
         if 11 in comp_hand and y < 31:
             x = comp_hand.index(11)
             comp_hand[x] = 1
             dealer_q(player_hand, comp_hand)
         else:
-            print('The computer went over. You win!')
-            print(declaration(player_hand, comp_hand, True))
-            start_game(False)
+            print(f'The computer went over. You win! \n {final_dec(player_hand, comp_hand)}')
+            restart_game()
     elif y > x:
-        print('The dealer has a higher score than you. You lose.')
-        print(declaration(player_hand, comp_hand, True))
-        start_game(False)
+        print(f'The dealer has a higher score than you. You lose. \n {final_dec(player_hand, comp_hand)}')
+        restart_game()
     elif y < 17:
         comp_hand.append(r.choice(cards))
         dealer_q(player_hand, comp_hand)
     elif y == x:
-        print('You got the same score as the dealer. You draw.')
-        print(declaration(player_hand, comp_hand, True))
+        print(f'You got the same score as the dealer. You draw. \n {final_dec(player_hand, comp_hand)}')
+        restart_game()
     else:
-        print('You have a higher score than the dealer. You win!')
-        print(declaration(player_hand, comp_hand, True))
-        start_game(False)
+        print(f'You have a higher score than the dealer. You win! \n {final_dec(player_hand, comp_hand)}')
+        restart_game()
 
 
 def calculate_score(player_hand, comp_hand):
@@ -113,13 +116,13 @@ def calculate_score(player_hand, comp_hand):
             calculate_score(player_hand, comp_hand)
         else:
             print('You went over 21 and lost')
-            print(declaration(player_hand, comp_hand, True))
-            start_game(False)
+            print(final_dec(player_hand, comp_hand))
+            restart_game()
     elif player_hand == 21:
-        print(declaration(player_hand, comp_hand, False))
+        print(current_dec(player_hand, comp_hand))
         dealer_q(player_hand, comp_hand)
     else:
-        print(declaration(player_hand, comp_hand, False))
+        print(current_dec(player_hand, comp_hand))
         card_q(player_hand, comp_hand)
 
 
